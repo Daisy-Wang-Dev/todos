75f5f2fe0a2e9033c5a0342eb293c74d84d21c16
Xueying Wang <daiszws19@gmail.com>
2023-12-15 19:19:41 +0000

    Add backgroud color to the app

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
 import "./Header.scss";
 
 const Header: React.FC = () => {
-    return (
-        <header className="header">
-            HAVE-TO-DOS!
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__name">HAVE-TO-DOS!</h1>
         </header>
-
-    )
-
-    
-}
+    </>
+  );
+};
 
 export default Header;

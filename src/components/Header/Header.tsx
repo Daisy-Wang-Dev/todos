97b590a664dd69abe0e6f619fcc5d4984cad8890
Xueying Wang <daiszws19@gmail.com>
2023-12-15 19:57:52 +0000

    Add more style to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import "./Header.scss";
 
 const Header: React.FC = () => {
+  const today = new Date();
   return (
     <>
       <header className="header">
         <h1 className="header__name">HAVE-TO-DOS!</h1>
-        </header>
+        <h4 className="header__date">
+          {today.getFullYear()}/ {today.getMonth()}/ {today.getDay()}
+        </h4>
+      </header>
     </>
   );
 };

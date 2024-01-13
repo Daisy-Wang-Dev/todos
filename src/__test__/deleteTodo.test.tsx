be95e17acf8dae8f48c7e7bb4bc8d0da19b0a4f4
Xueying Wang <daiszws19@gmail.com>
2024-01-13 18:59:24 +0000

    Add test to check if all delete buttons are working

--- src/__test__/deleteTodo.test.tsx
+++ src/__test__/deleteTodo.test.tsx
@@ -1,12 +1,11 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../App";
-import {addTask} from "./functions";
-
+import { addTask } from "./functions";
 
 describe("App", () => {
-  it("deletes a new task correctly", () => {
+  it("deletes an added task correctly", () => {
     render(<App />);
-    addTask(["Go grocery shopping"])
+    addTask(["Go grocery shopping"]);
     const taskElement = screen.getByText("Go grocery shopping");
     expect(taskElement).toBeInTheDocument();
     const deleteButtonElement = screen.getByTitle("delete");
@@ -15,10 +14,21 @@ describe("App", () => {
     expect(deletedTask).not.toBeInTheDocument();
   });
 
-//   it("adds all tasks correctly", () => {
-//     render(<App />);
-//     addTask(["Go grocery shopping", "Pet lizzy's cat", "Do the dishes"])
-//     const taskElements = screen.getAllByTestId("taskItem");
-//     expect(taskElements.length).toBe(3);
-//   });
+  it("deletes all added tasks", () => {
+    render(<App />);
+    const task: string[] = [
+      "Go grocery shopping",
+      "Pet lizzy's cat",
+      "Do the dishes",
+    ];
+    addTask(task);
+    const taskElements = screen.getAllByTestId("taskItem");
+    expect(taskElements.length).toBe(task.length);
+    const deleteButtonElements = screen.getAllByTitle("delete");
+    for (let i = 0; i < task.length; i++) {
+      deleteButtonElements.forEach((but) => fireEvent.click(but));
+    }
+    const deletedTasks = screen.queryAllByTestId("taskItem");
+    expect(deletedTasks).not.toBe(0);
+  });
 });

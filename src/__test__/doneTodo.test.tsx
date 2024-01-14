6a3bc986f18fdcc1467b4696e7915e08d9c1605b
Xueying Wang <daiszws19@gmail.com>
2024-01-14 21:09:30 +0000

    Add test to ensure a task can be marked as done

--- src/__test__/doneTodo.test.tsx
+++ src/__test__/doneTodo.test.tsx
@@ -10,27 +10,10 @@ describe("App", () => {
     render(<App />);
     addTask([task]);
     findOneTaskEl(task);
-    const deleteButtonElement = screen.getByTitle("delete");
-    fireEvent.click(deleteButtonElement);
-    const deletedTask = screen.queryByText("Go grocery shopping");
-    expect(deletedTask).not.toBeInTheDocument();
+    const doneButtonElement = screen.getByTitle("done");
+    fireEvent.click(doneButtonElement);
+    const doneTask = screen.getByText("Go grocery shopping");
+    expect(doneTask).toHaveClass("todo__single--done");
   });
 
-  it("deletes all added tasks", () => {
-    render(<App />);
-    const task: string[] = [
-      "Go grocery shopping",
-      "Pet lizzy's cat",
-      "Do the dishes",
-    ];
-    addTask(task);
-    const taskElements = screen.getAllByTestId("taskItem");
-    expect(taskElements.length).toBe(task.length);
-    const deleteButtonElements = screen.getAllByTitle("delete");
-    for (let i = 0; i < task.length; i++) {
-      deleteButtonElements.forEach((but) => fireEvent.click(but));
-    }
-    const deletedTasks = screen.queryAllByTestId("taskItem");
-    expect(deletedTasks).not.toBe(0);
-  });
 });
